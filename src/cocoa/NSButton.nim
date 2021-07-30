ffa7d27690e28936a0cd363835051cdab75a5ac6
Armando Rivera <airrsongs@gmail.com>
2021-07-29 21:31:42 -0400

    Added comments

--- src/cocoa/NSButton.nim
+++ src/cocoa/NSButton.nim
@@ -3,3 +3,18 @@
 import NSFunctions
 
 proc newButton*(parent: ID; caption: cstring; left: cint; top: cint; width: cint; height: cint; `func`: ACTION): ID  {.cdecl, importc: "createButton".}
+## Creates new Button object
+##
+## Example:
+## 
+## .. code-block::nim
+##    btn1 = newButton(mainWindow, "Open", width-100, 20, 90, 24, btnClicked)
+## 
+## 
+## Callback must be in the form:
+##
+## .. code-block::nim
+##    proc buttonCB(sender: ID) {.cdecl.} =
+##      <..code...>
+##
+

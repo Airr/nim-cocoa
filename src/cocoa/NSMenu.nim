40311dab753504a1ccff834e84c1fdfd20148bd0
Armando Rivera <airrsongs@gmail.com>
2021-08-03 22:24:15 -0400

    Added autogeneration of Menus via newEditMenu/newFormatMenu

--- src/cocoa/NSMenu.nim
+++ src/cocoa/NSMenu.nim
@@ -5,3 +5,7 @@ import NSFunctions
 proc newMenu*(title: cstring): ID {.cdecl, importc: "createMenu".}
 proc newMenuItem*(parent: ID, caption, key: cstring, `callback`:ACTION) {.cdecl, importc: "createMenuItem".}
 proc newMenuSeparator*(parent: ID) {.cdecl, importc: "createMenuSeparator".}
+proc setSystemAction*(widget: ID, title, actionName: cstring) {.cdecl, importc: "setSystemAction".}
+proc newFileMenu*() {.cdecl, importc: "createFileMenu".}
+proc newEditMenu*() {.cdecl, importc: "createEditMenu".}
+proc newFormatMenu*() {.cdecl, importc: "createFormatMenu".}
